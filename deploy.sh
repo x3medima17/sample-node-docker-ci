e9571d205e60cbec260631dd1e48ce6b30b84996
Kostya <kostyaurysov@gmail.com>
2017-02-28 14:48:40 +0300

    bug fixing

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ docker stop web || true
 docker rm web || true
 docker rmi kostyaurysov/sample-node:current || true
 docker tag kostyaurysov/sample-node:latest kostyaurysov/sample-node:current
-docker run -d --net app --restart always --name web -p 3000:3000 kostyaurysov/sample-node:current
+docker run -d --net app --restart always --name web -p 80:80 kostyaurysov/sample-node:current
 EOF
