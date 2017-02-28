db1806b5d04337d7404a5e88085564add99725f6
Kostya <kostyaurysov@gmail.com>
2017-02-28 14:13:08 +0300

    changes

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # docker build -t kostyaurysov/sample-node .
 docker push kostyaurysov/sample-node
 
-ssh -i deploy deploy@35.187.30.81 << EOF
+ssh deploy@35.187.30.81 << EOF
 docker pull kostyaurysov/sample-node:latest
 docker stop web || true
 docker rm web || true
