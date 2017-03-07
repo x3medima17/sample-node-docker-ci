5e9c70a645adfd9979a977ec5f1e2739f84784f5
Your Name <you@example.com>
2017-03-07 10:20:54 +0000

    ok

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ docker stop web || true
 docker rm web || true
 docker rmi x3medima17/sample-node:current || true
 docker tag x3medima17/sample-node:latest x3medima17/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 x3medima17/sample-node:current
+docker run -d --net cc --restart always --name web -p 80:80 x3medima17/sample-node:current
 EOF
